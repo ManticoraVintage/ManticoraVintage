a5beb9a31b3751f2b7d418573243d7a86af67cd3
Julen <julen.castillo@ikasle.egibide.org>
2021-02-19 20:39:09 +0100

    Preparing the main layout
    Preparing the main layout of the Home component

--- manticoravintage/resources/js/components/Home/Home.vue
+++ manticoravintage/resources/js/components/Home/Home.vue
@@ -1,9 +1,24 @@
 <template>
-    <div class="main"><p>Home</p></div>
+    <div class="main">
+        <div class="page-title-container d-flex align-items-center">MANTICORA VINTAGE</div>
+    </div>
 </template>
+
+<style scoped>
+.main{
+    width: 100vw;
+}
+.page-title-container{
+    width: 100%;
+    padding: 10px;
+    font-size: 35px;
+    
+}
+</style>
 
 <script>
 export default {
     
 }
 </script>
+
--- manticoravintage/storage/framework/views/55e65c38eeb8a7bb183a7fe985ce537cea18163d.php
+++ manticoravintage/storage/framework/views/55e65c38eeb8a7bb183a7fe985ce537cea18163d.php
@@ -8,6 +8,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
         <script src="<?php echo e(asset('js/app.js')); ?>" defer></script>
         <!-- Styles -->
         <style>
