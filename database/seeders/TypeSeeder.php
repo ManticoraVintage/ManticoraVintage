f3a2601505fc6c369eaf75755b65945549f2f196
AdrianDanlos <danlosadrian@gmail.com>
2021-05-13 19:40:45 +0700

    type descriptions added

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -17,17 +17,13 @@ class TypeSeeder extends Seeder
      */
     public function run()
     {
-        // $types = array('Selected', 'Standard', 'Tangas');
-
-        // foreach ($types as $type) {
-        //     Type::factory()->count(1)->create(['name' => $type]);
-        // }
-
+        $zozobraDescription = "Zozobra marks the beginning of her values with thongs. A fetishized product and always linked to the feminine. The idea is to approach it from a point of view that does not encompass gender and that focuses on the body. And thus turn this garment into a liberating product that is provocative and fun. From the classic thong to our own double strap design thong, perfect for showing it off wherever you go. We created the same design but with two variations, one for the penis and the other for the vagina. And at the same time we offer the possibility of superimposing the two designs (the single thong + the double strap) giving the opportunity to experiment with shapes and create different variations to suit each individual's taste. In addition to offering a range of colors that promises to grow and grow, the same brand also promises to grow and evolve with new designs and garments with the same values as thongs.";
+        $vintageDescription = "The two thousand have always been criticized in the fashion world for their risky combinations and bizarre outfits; low-rise pants, mini-skirts, bright colors ... Manticora.Vntg aims to bring back that cheeky and provocative essence, giving value to all those clothing brands with their own style that were devoured by multinationals. Miss Sixty, Morgan de toi, Lois, Baby Phat ... Brands that today have lost all they were to lose themselves in a commercial and decaf style. Our task is to find those unique pieces that marked a decade, which most of them manufactured in their countries of origin (and not in countries where mass production or exploitation predominates). In addition, Manticora.vntg also offers a selection of brands from great designers that were everything in the two thousand.Manticora is committed to experimenting and leaving aside the stigmas created by clothing divided by genres and that each one risks and causes as they please.";
+        
         DB::table('types')->insert([
-            ['id' => 1, 'created_at' => Carbon::create('2021', '01', '01'), 'updated_at' => Carbon::create('2021', '01', '01'), 'name' => 'vintage'],
-            ['id' => 2, 'created_at' => Carbon::create('2021', '01', '01'), 'updated_at' => Carbon::create('2021', '01', '01'), 'name' => 'selected'],
-            ['id' => 3, 'created_at' => Carbon::create('2021', '01', '01'), 'updated_at' => Carbon::create('2021', '01', '01'), 'name' => 'zozobra'],
-
+            ['id' => 1, 'created_at' => Carbon::create('2021', '01', '01'), 'updated_at' => Carbon::create('2021', '01', '01'), 'name' => 'vintage', 'description' => $vintageDescription],
+            ['id' => 2, 'created_at' => Carbon::create('2021', '01', '01'), 'updated_at' => Carbon::create('2021', '01', '01'), 'name' => 'selected', 'description' => ''],
+            ['id' => 3, 'created_at' => Carbon::create('2021', '01', '01'), 'updated_at' => Carbon::create('2021', '01', '01'), 'name' => 'zozobra', 'description' => $zozobraDescription],
         ]);
     }
 }
