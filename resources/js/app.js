307e9ad388ec297aa93994ae4f8083808f8dfa7d
Julen <julen.castillo@ikasle.egibide.org>
2021-05-26 18:33:29 +0200

    Revert "Details buttons added"
    This reverts commit 153b9a1fde18354fe413ae16f57a2e69f47e13df.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,32 +1,28 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+require("./bootstrap");
 
-require('./bootstrap');
+import router from "./routes";
+import VueRouter from "vue-router";
+import Vue from "vue";
+import Modal from "@burhanahmeed/vue-modal-2";
+import VueCarousel from 'vue-carousel';
 
-window.Vue = require('vue').default;
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.use(Modal, {
+  componentName: "ModalVue"
+});
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+window.Vue = require("vue").default;
+
+Vue.component("index", require("./components/Index.vue").default);
+//Vue.component('admin', require('./components/Admin/Admin.vue'));
+
+Vue.use(VueRouter);
+Vue.use(VueCarousel);
+
 
 const app = new Vue({
-    el: '#app',
+    el: "#app",
+    router
 });
