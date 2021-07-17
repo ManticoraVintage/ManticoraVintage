2248c08f1375a4ce4082d373b02ce9c41a50fd82
JulenC <julencastillo98@gmail.com>
2021-07-17 17:24:52 +0200

    Testing2.0

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,7 +23,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet" meta
         http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ secure_asset('js/app.js') }}" defer></script>
     <!-- Styles -->
     <style>
         /*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */
